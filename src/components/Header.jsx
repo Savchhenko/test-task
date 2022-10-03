a3b6783739e918df6455c35a4adc3e4075ad1ce3
Alexandra Savchenko <svchhh@yandex.ru>
2022-10-03 19:06:40 +0300

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import cartIcon from "../images/cart-icon.svg";
+import likesIcon from "../images/likes-icon.svg";
+import "../index.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className="logo">QPICK</div>
+        <div className="header__icons">
+            <a href="#" className="icon">
+                <span className="icon-notification">2</span>
+                <img src={likesIcon} alt="likes icon" />
+            </a>
+            <a href="#" className="icon">
+                <span className="icon-notification">1</span>
+                <img src={cartIcon} alt="cart icon"/>
+            </a>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
