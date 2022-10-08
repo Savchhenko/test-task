fbc74dfebb80a532250b0dc9d1ee23f363294d63
Alexandra Savchenko <svchhh@yandex.ru>
2022-10-08 16:49:34 +0300

    add Products2 component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./index.css";
 import Header from "./components/Header";
 import Products1 from "./components/Products1";
+import Products2 from "./components/Products2";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Products1 />
+      <Products2 />
     </div>
   );
 }
