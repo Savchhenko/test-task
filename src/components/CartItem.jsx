35167ca6daf50b2959a7fea8bd8b2f569308e674
Alexandra Savchenko <svchhh@yandex.ru>
2022-10-12 00:43:58 +0300

    add counter for cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,26 +1,42 @@
 import "../index.css";
+import { useState } from "react";
 
 const CartItem = (props) => {
     const { item } = props;
+    
+    const [counterNumber, setCounterNumber] = useState(1);
+    const [productsCost, setProductsCost] = useState(item.price);
+
+    const minusHandler = () => {
+        if (counterNumber > 1) {
+            setCounterNumber(counterNumber - 1);
+            setProductsCost(productsCost - item.price);
+        } 
+    };
+    const plusHandler = () => {
+        setCounterNumber(counterNumber + 1);
+        setProductsCost(productsCost + item.price);
+    };
+
     return (
         <div className="cart__item cart__product">
             <div className="cart__product-img">
                 <img src={item.img} />
                 <div className="counter">
-                    <button className="counter-btn"><span className="circle minus"></span></button>
-                    <span>1</span>
-                    <button className="counter-btn"><span className="circle plus"></span></button>
+                    <button onClick={minusHandler} className="counter-btn"><span className="circle minus"></span></button>
+                    <span>{counterNumber}</span>
+                    <button onClick={plusHandler} className="counter-btn"><span className="circle plus"></span></button>
                 </div>
             </div>
             <div className="cart__product-info">
                 <div>
                     <h4>{item.title}</h4>
-                    <span>{item.price}</span>
+                    <span>{item.price} P</span>
                 </div>
             </div>
             <div className="cart__product-entirecost">
                 <img src="./images/delete-icon.svg" />
-                <span><b>2 927 ₽</b></span>
+                <span><b>{productsCost} P</b></span>
             </div>
         </div>
     );
